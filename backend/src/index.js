967f92ba21914fcd12c3a1faf4de267ae108976f
yojiong <2293497608@qq.com>
2026-05-16 23:36:45 +0800

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const Alert = require('./models/Alert');
 
 const app = express();
 app.use(cors({
-  origin: 'https://health_companion-1-nfds.onrender.com',  // 你的前端域名
+  origin: 'https://health-companion-1-nfds.onrender.com',  // 你的前端域名
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
