33fd1b3ddd1ac72d067abb894a4fc6ac7b9cbdc0
yojiong <2293497608@qq.com>
2026-05-16 23:47:18 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://health_companion-2.onrender.com',
+        target: 'https://health-companion-2.onrender.com',
         changeOrigin: true
       }
     }
